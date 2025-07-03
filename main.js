05cb7617158118d05126690b4c59402dfe98879c
Shakhnoza <sherova1994@gmail.com>
2025-07-03 10:29:54 +0500

    final

---
+++ main.js
@@ -0,0 +1,27 @@
+
+var name = prompt ('Введите имя')
+console.log(name);
+
+var age = +prompt ('Введите возраст')
+console.log(age);
+
+console.log('Меня зовут ' + name + ' мне ' + age);
+
+var task = prompt ('Решите пример 15 + 5')
+console.log('15 + 5 = ' + (15 + 5));
+var task = prompt ('Решите пример 20 - 8')
+console.log('20 - 8 = ' + (20 - 8));
+var task = prompt ('Решите пример 12 * 5')
+console.log('12 * 5 = ' + 12 * 5);
+var task = prompt ('Решите пример 12 / 2')
+console.log('12 / 2 = ' + 12 / 2);
+var task = prompt ('Решите пример 12 % 2')
+console.log('12 % 2 = ' + 12 % 2);
+
+var X, Y, Z, res
+var X = prompt ('Введите первое число')
+var Y = prompt ('Введите второе число')
+var Z = prompt ('Введите третье число')
+var res = prompt (X+Y+Z/3)
+alert(res)
+
