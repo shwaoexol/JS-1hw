6a8636049927e52cba78f2f2087314323467c13d
Shakhnoza <sherova1994@gmail.com>
2025-07-11 11:45:19 +0500

    fix-errors

--- main.js
+++ main.js
@@ -1,27 +1,31 @@
 
-var name = prompt ('Введите имя')
+var name = prompt ('Введите имя');
 console.log(name);
 
-var age = +prompt ('Введите возраст')
+var age = +prompt ('Введите возраст');
 console.log(age);
 
-console.log('Меня зовут ' + name + ' мне ' + age);
+console.log('Меня зовут ' + name + ', мне ' + age + ' лет.');
 
-var task = prompt ('Решите пример 15 + 5')
-console.log('15 + 5 = ' + (15 + 5));
-var task = prompt ('Решите пример 20 - 8')
-console.log('20 - 8 = ' + (20 - 8));
-var task = prompt ('Решите пример 12 * 5')
-console.log('12 * 5 = ' + 12 * 5);
-var task = prompt ('Решите пример 12 / 2')
-console.log('12 / 2 = ' + 12 / 2);
-var task = prompt ('Решите пример 12 % 2')
-console.log('12 % 2 = ' + 12 % 2);
+var task1 = +prompt('Решите пример 15 + 5');
+console.log('15 + 5 = ' + (15 + 5) + ', Ваш ответ: ' + task1);
+
+var task2 = +prompt('Решите пример 20 - 8');
+console.log('20 - 8 = ' + (20 - 8) + ', Ваш ответ: ' + task2);
+
+var task3 = +prompt('Решите пример 12 * 5');
+console.log('12 * 5 = ' + 12 * 5 + ', Ваш ответ: ' + task3);
+
+var task4 = +prompt('Решите пример 12 / 2');
+console.log('12 / 2 = ' + 12 / 2 + ', Ваш ответ: ' + task4);
+
+var task5 = +prompt('Решите пример 12 % 2');
+console.log('12 % 2 = ' + 12 % 2 + ', Ваш ответ: ' + task5);
 
 var X, Y, Z, res
-var X = prompt ('Введите первое число')
-var Y = prompt ('Введите второе число')
-var Z = prompt ('Введите третье число')
-var res = prompt (X+Y+Z/3)
-alert(res)
+var X = +prompt ('Введите первое число');
+var Y = +prompt ('Введите второе число');
+var Z = +prompt ('Введите третье число');
+var res = (X + Y + Z / 3);
+alert('Среднее арифметическое' +  res );
 
